565c25708c65b7bb7d7994b476b825d34cce8c23
Mikhail <riddler2077@gmail.com>
2023-03-24 19:48:22 +0400

    v 0.5

--- ASFGenerator/Program.cs
+++ ASFGenerator/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Media3D;
@@ -21,7 +22,7 @@ namespace ASFGenerator
 
         static string config = String.Format("{0}\\config", Environment.CurrentDirectory);
 
-        static int rowskip = 0;
+        static int rowskip = 1;
         static int name = 0;
         static int login = 0;
         static int password = 0;
@@ -43,7 +44,8 @@ namespace ASFGenerator
             Console.Write("Select language (default EN):");
             result = Console.ReadLine();
             if (result.ToLower().StartsWith("en") || string.IsNullOrEmpty(result) || string.IsNullOrWhiteSpace(result)) language = english;
-            else language = russian;
+            else if(result.ToLower().StartsWith("ru")) language = russian;
+            else language = english;
 
             Console.Write(language["gameForIdle"]);
             result = Console.ReadLine();
@@ -52,7 +54,7 @@ namespace ASFGenerator
             Console.Write(language["skipHeaders"]);
             Console.SetCursorPosition(language["skipHeaders"].Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)[0].Length, (Console.CursorTop - 1));
             result = Console.ReadLine();
-            if (!Int32.TryParse(result, out rowskip)) rowskip = 0;
+            if (!Int32.TryParse(result, out rowskip)) rowskip = 1;
             ClearNote(language["skipHeaders"]);
 
             Console.Write(language["columnName"]);
@@ -79,7 +81,8 @@ namespace ASFGenerator
 
             foreach (var account in model)
             {
-                Console.WriteLine("{0} {1} {2} {3}", account.name, account.login, account.password, account.token);
+                if (language["language"] == "en") Console.WriteLine("Created {0}.json with data {1} {2} {3}", account.name, account.login, account.password, account.token);
+                else Console.WriteLine("Создан {0}.json с данными {1} {2} {3}", account.name, account.login, account.password, account.token);
 
                 string filename = String.Format("{0}\\{1}.json", config, account.name.Replace(" ", ""));
                 using (var writer = new StreamWriter(filename, false))
@@ -103,11 +106,12 @@ namespace ASFGenerator
                 foreach (var file in Directory.GetFiles(mafiles))
                 {
                     string fileText = File.ReadAllText(file);
-                    if (fileText.Contains(String.Format("account_name\":\"{0}", account.login.ToLower())))
+                    if (fileText.Contains(String.Format("account_name\":\"{0}\"", account.login.ToLower())))
                     {
                         string tofile = String.Format("{0}\\{1}.maFile", config, account.name.Replace(" ", ""));
                         File.Copy(file, tofile, true);
-                        Console.WriteLine("{0} SDA Cкопирован!", account.name);
+                        if (language["language"] == "en") Console.WriteLine("{0} maFile found. Copied to config.", account.name);
+                        else Console.WriteLine("{0} maFile найден. Скопирован в config.", account.name);
                         break;
                     }
                 }
@@ -123,25 +127,26 @@ namespace ASFGenerator
         }
 
         private static Dictionary<string, string> english = new Dictionary<string, string>
-            {
-                {"gameForIdle", "Enter the game number for idle (default 730):"},
-                {"skipHeaders", String.Format("How many lines need to skip from the top (default 0):{0}Note: value to skip headers. If there are no headers - leave input is empty", Environment.NewLine) },
-                {"columnName", String.Format("Enter the number of the column with nicknames:{0}Note: leave the input blank if you want the nickname to be the same as the login", Environment.NewLine) },
-                {"columnLogin", "Enter the number of the column with logins (default 1):" },
-                {"columnPassword", "Enter the number of the column with passwords (default 2):" },
-                {"columnToken", "Enter the number of the column with tokens (default 3):" }
-            };
+        {
+            {"language", "en" },
+            {"gameForIdle", "Enter the game number for idle (default 730):"},
+            {"skipHeaders", String.Format("How many lines need to skip from the top (default 1):{0}Note: value to skip headers. If there are no headers - leave input is empty", Environment.NewLine) },
+            {"columnName", String.Format("Enter the number of the column with nicknames:{0}Note: leave the input blank if you want the nickname to be the same as the login", Environment.NewLine) },
+            {"columnLogin", "Enter the number of the column with logins (default 1):" },
+            {"columnPassword", "Enter the number of the column with passwords (default 2):" },
+            {"columnToken", "Enter the number of the column with tokens (default 3):" }
+        };
 
         private static Dictionary<string, string> russian = new Dictionary<string, string>
-            {
-                {"gameForIdle", "Введите номер игры для фарма часов (по умолчанию 730):"},
-                {"skipHeaders", String.Format("Сколько строк нужно отступить сверху (по умолчанию 0):{0}Подсказка: значение отступа для пропуска заголовков. Если заголовков нет - оставьте строку пустой", Environment.NewLine) },
-                {"columnName", String.Format("Введите номер столбца с никами:{0}Подсказка: оставьте ввод пустым если хотите чтобы ник был таким же как и логин", Environment.NewLine) },
-                {"columnLogin", "Введите номер столбца с логинами (по умолчанию 1):" },
-                {"columnPassword", "Введите номер столбца с паролями (по умолчанию 2):" },
-                {"columnToken", "Введите номер столбца с токенами (по умолчанию 3):" }
-
-            };
+        {
+            {"language", "ru" },
+            {"gameForIdle", "Введите номер игры для фарма часов (по умолчанию 730):"},
+            {"skipHeaders", String.Format("Сколько строк нужно отступить сверху (по умолчанию 1):{0}Подсказка: значение отступа для пропуска заголовков. Если заголовков нет - оставьте строку пустой", Environment.NewLine) },
+            {"columnName", String.Format("Введите номер столбца с никами:{0}Подсказка: оставьте ввод пустым если хотите чтобы ник был таким же как и логин", Environment.NewLine) },
+            {"columnLogin", "Введите номер столбца с логинами (по умолчанию 1):" },
+            {"columnPassword", "Введите номер столбца с паролями (по умолчанию 2):" },
+            {"columnToken", "Введите номер столбца с токенами (по умолчанию 3):" }
+        };
 
         #region ReadFromExcel
         private static List<Model> ReadFromExcel(string FilePath)
@@ -160,7 +165,7 @@ namespace ASFGenerator
 
                 List<Model> list = new List<Model>();
 
-                for (int r = rowskip; r < rowCount; r++) 
+                for (int r = rowskip; r < (rowCount + 1); r++) 
                 {
                     Model model = new Model();
                     try
@@ -171,7 +176,7 @@ namespace ASFGenerator
                         model.login = worksheet.Cells[r, login].Value.ToString();
                         if (worksheet.Cells[r, password].Value == null) continue;
                         model.password = worksheet.Cells[r, password].Value.ToString();
-                        if (worksheet.Cells[r, token].Value == null) continue;
+                        if (worksheet.Cells[r, token].Value == null) { list.Add(model); continue; }
                         model.token = worksheet.Cells[r, token].Value.ToString();
                         list.Add(model);
                     }
